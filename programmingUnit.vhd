cbab87430b3a81b98680adcf79665ed1bebfae40
Alexander Mot <asd777101@gmail.com>
2020-07-28 23:08:24 -0700

    Attempt to sync done signal with all other outputs being ready

--- programmingUnit.vhd
+++ programmingUnit.vhd
@@ -22,6 +22,9 @@ PORT (clock, reset, enable : IN STD_LOGIC;
 END COMPONENT;
 
 SIGNAL setAsserted : STD_LOGIC := '0';				-- whether or not set has been asserted
+SIGNAL doneWait :	STD_LOGIC := '0';					-- signal to indicate that the circuit is done and waiting for the data
+SIGNAL doneWaitCounter : INTEGER := 2;				-- number of clock cycles left until data is ready
+
 SIGNAL enableAccumulator : STD_LOGIC;				-- enables or disables the accumulator
 SIGNAL accumulatorOut : UNSIGNED(5 DOWNTO 0);	-- output from the accumulator
 
@@ -30,14 +33,22 @@ BEGIN
 	PROCESS(clock)
 	BEGIN
 		IF (rising_edge(clock)) THEN
-			IF (set = '1' OR setAsserted = '1') THEN -- if set is (or has been) asserted
+			IF (doneWait = '1') THEN			-- if the circuit is done and waiting for the data
+				IF (doneWaitCounter = 0) THEN
+					done <= '1';					-- if clock cycles left until data is ready is 0, assert done
+					doneWaitCounter <= 2;		-- reset counter
+				ELSE
+					doneWaitCounter <= doneWaitCounter - 1;	-- else, decrement the counter
+				END IF;
+			
+			ELSIF (set = '1' OR setAsserted = '1') THEN -- else if set is (or has been) asserted
 				setAsserted <= '1';				-- set setAsserted to 1
 				enableAccumulator <= '0';		-- assume the accumulator needs to be turned off
 				
 				writeEnable <= '1';
 				address <= product;
 				data <= accumulatorOut;
-				done <= '1';
+				doneWait <= '1';
 			ELSE
 				enableAccumulator <= enable;
 			END IF;
