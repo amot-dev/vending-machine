61b3dfdee0483b42963392116163a0714c866e94
Alexander Mot <asd777101@gmail.com>
2020-07-28 19:45:27 -0700

    Add base for programming unit

---
+++ programmingUnit.vhd
@@ -0,0 +1,11 @@
+ENTITY programmingUnit IS
+PORT (clock, reset, hardReset, set, enable, product : IN STD_LOGIC;
+		NDQ														 : IN STD_LOGic_VECTOR(2 DOWNTO 0);
+		writeEnable												 : OUT STD_LOGIC;
+		data, address											 : OUT UNSIGNED(5 DOWNTO 0));
+END accumulator;
+
+ARCHITECTURE Behaviour OF programmingUnit IS
+
+BEGIN
+END Behaviour;
