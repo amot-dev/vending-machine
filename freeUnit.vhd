001a1579a4996cef9c464638cf95cf8933110663
Alexander Mot <asd777101@gmail.com>
2020-07-31 00:28:06 -0700

    Change product to price to match vending unit

--- freeUnit.vhd
+++ freeUnit.vhd
@@ -4,12 +4,12 @@ USE Ieee.numeric_std.all;
 
 
 ENTITY freeUnit IS
-PORT (clock, reset, enable : IN STD_LOGIC;
-		product					: IN STD_LOGIC_VECTOR(1 DOWNTO 0);
-		QDN						: IN STD_LOGic_VECTOR(2 DOWNTO 0);
-		totalInserted			: OUT UNSIGNED(5 DOWNTO 0);
-		change					: OUT UNSIGNED(5 DOWNTO 0);
-		done						: OUT STD_LOGIC);
+PORT (clock, enable : IN STD_LOGIC;
+		price			  : IN UNSIGNED(5 DOWNTO 0);
+		QDN			  : IN STD_LOGic_VECTOR(2 DOWNTO 0);
+		totalInserted : OUT UNSIGNED(5 DOWNTO 0);
+		change		  : OUT UNSIGNED(5 DOWNTO 0);
+		done			  : OUT STD_LOGIC);
 END freeUnit;
 
 ARCHITECTURE Behaviour OF freeUnit IS
