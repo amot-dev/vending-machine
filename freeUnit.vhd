853dacbfd2b30654629252a9c09da0a43f425f0a
Alexander Mot <asd777101@gmail.com>
2020-07-31 00:21:31 -0700

    Touch up free unit Remove unnecessary inputs and create condition to unassert done

--- freeUnit.vhd
+++ freeUnit.vhd
@@ -14,15 +14,23 @@ END freeUnit;
 
 ARCHITECTURE Behaviour OF freeUnit IS
 
+SIGNAL doneAsserted : STD_LOGIC := '0';-- whether or not done is asserted
+
 BEGIN
 
 	PROCESS(clock)
-	BEGIN
-		IF rising_edge(clock) THEN
-			totalInserted <= "ZZZZZZ";	-- as this unit dispenses products for free, set money-related outputs to floating
-			change <= "ZZZZZZ";
-			done <= '1';					-- dispense product
+	BEGIN											-- no reset is needed for this circuit (not even need for a reset input)
+		IF (rising_edge(clock) and enable = '1') THEN
+			IF (doneAsserted <= '1') THEN	-- unassert done after one clock cycle
+				doneAsserted <= '0';
+			ELSE
+				totalInserted <= "ZZZZZZ";	-- as this unit dispenses products for free, set money-related outputs to floating
+				change <= "ZZZZZZ";
+				doneAsserted <= '1';			-- dispense product
+			END IF;
 		END IF;
 	END PROCESS;
+	
+	done <= doneAsserted;
 
 END Behaviour;
