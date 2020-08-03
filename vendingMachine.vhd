0873af0dcccb680621d8b44663bfa5ad9f2f87f3
Alexander Mot <asd777101@gmail.com>
2020-08-03 12:11:49 -0700

    Set up template for vending machine controller

---
+++ vendingMachine.vhd
@@ -0,0 +1,45 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE Ieee.numeric_std.all;
+
+
+ENTITY vendingMachine IS
+PORT (clock, reset, hard_reset, start, funct, prod, set : IN STD_LOGIC;
+		Q, D, N														  : IN STD_LOGic;
+		change0, change1, change2								  : OUT UNSIGNED(3 DOWNTO 0);
+		runTotal0, runTotal1, runTotal2						  : OUT UNSIGNED(3 DOWNTO 0);
+		total0, total1, total2									  : OUT UNSIGNED(3 DOWNTO 0);
+END vendingMachine;
+
+ARCHITECTURE Behaviour OF vendingMachine IS
+
+COMPONENT programmingUnit IS
+PORT (clock, reset, set, enable : IN STD_LOGIC;
+		product						  : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+		QDN							  : IN STD_LOGic_VECTOR(2 DOWNTO 0);
+		writeEnable					  : OUT STD_LOGIC;
+		address						  : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+		data				           : OUT UNSIGNED(5 DOWNTO 0);
+		done							  : OUT STD_LOGIC);
+END COMPONENT;
+
+COMPONENT vendingUnit IS
+PORT (clock, reset, enable : IN STD_LOGIC;
+		price						: IN UNSIGNED(5 DOWNTO 0);
+		QDN						: IN STD_LOGic_VECTOR(2 DOWNTO 0);
+		totalInserted,change	: OUT UNSIGNED(5 DOWNTO 0);
+		done						: OUT STD_LOGIC);
+END COMPONENT;
+
+COMPONENT freeUnit IS
+PORT (clock, enable : IN STD_LOGIC;
+		price			  : IN UNSIGNED(5 DOWNTO 0);
+		QDN			  : IN STD_LOGic_VECTOR(2 DOWNTO 0);
+		totalInserted : OUT UNSIGNED(5 DOWNTO 0);
+		change		  : OUT UNSIGNED(5 DOWNTO 0);
+		done			  : OUT STD_LOGIC);
+END COMPONENT;
+
+BEGIN
+
+END Behaviour;
