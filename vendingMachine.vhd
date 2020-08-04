ba13d402fd2f8627feec569eb5e9ccc2970a8568
Alexander Mot <asd777101@gmail.com>
2020-08-03 19:34:20 -0700

    Complete vending machine controller code

--- vendingMachine.vhd
+++ vendingMachine.vhd
@@ -62,20 +62,29 @@ SIGNAL state : STD_LOGIC_VECTOR(2 DOWNTO 0) := "000";	-- the current state of th
 SIGNAL programmingEnable, vendingEnable, freeEnable : STD_LOGIC := '0';	-- enables for the various components
 SIGNAL allDone : STD_LOGIC := '0';							-- whether or not the current process is finished
 
-SIGNAL currentPrice : UNSIGNED(5 DOWNTO 0);				-- contains the price of the currently selected product
-SIGNAL currentPriceSTD : STD_LOGIC_VECTOR(5 DOWNTO 0);-- used for conversion from sram
+SIGNAL currentPrice 			 : UNSIGNED(5 DOWNTO 0);				-- contains the system price (a quarter is 5) of the currently selected product
+SIGNAL currentPriceSTD  	 : STD_LOGIC_VECTOR(5 DOWNTO 0);		-- used for conversion from sram
 
--- outputs from programming unit
+SIGNAL currentPriceReal 	 : UNSIGNED(8 DOWNTO 0);				-- stores the real price (a quarter is 25) of *various things*
+SIGNAL changeOutReal			 : UNSIGNED(8 DOWNTO 0);				-- must be 8 DOWNTO 0 because 5*(6-bit num) -> 9-bit num
+SIGNAL totalInsertedOutReal : UNSIGNED(8 DOWNTO 0);				-- of course, since we only represent values up to 255, we can trim off the msb after the fact
+
+-- programming unit
 SIGNAL dataOut : UNSIGNED(5 DOWNTO 0);
 SIGNAL addressOut : STD_LOGIC_VECTOR(1 DOWNTO 0);
 SIGNAL writeEnableOut : STD_LOGIC := '0';
 SIGNAL programmingDone : STD_LOGIC := '0';
+ 
+-- display unit
+SIGNAL displayOut0, displayOut1, displayOut2 : UNSIGNED(3 DOWNTO 0);
 
--- outputs from vending unit
+-- vending unit
 SIGNAL totalInsertedOut, changeOut : UNSIGNED(5 DOWNTO 0);
 SIGNAL vendingDone : STD_LOGIC := '0';
+SIGNAL changeOut0, changeOut1, changeOut2 		: UNSIGNED(3 DOWNTO 0);
+SIGNAL runTotalOut0, runTotalOut1, runTotalOut2 : UNSIGNED(3 DOWNTO 0);
 
---outputs from free unit
+-- free unit
 SIGNAL totalInsertedFree, changeFree : UNSIGNED(5 DOWNTO 0);
 SIGNAL freeDone : STD_LOGIC := '0';
 
@@ -111,34 +120,84 @@ BEGIN
 				programmingEnable <= '0';
 				vendingEnable <= '0';
 				freeEnable <= '0';
+				
+				change0 <= "0000";			-- set all outputs to 0
+				change1 <= "0000";
+				change2 <= "0000";
+				runTotal0 <= "0000";
+				runTotal1 <= "0000";
+				runTotal2 <= "0000";
+				total0 <= "0000";
+				total1 <= "0000";
+				total2 <= "0000";
 			ELSIF (state = "001") THEN
 				programmingEnable <= '1';	-- in programming state, enable programming
+													-- since the outputs are not modified by the programming state, they are all still 0
 			ELSIF (state = "010") THEN
-				-- do display stuff
+				total0 <= displayOut0;		-- in display state, output price of the currently selected product
+				total1 <= displayOut1;
+				total2 <= displayOut2;
 			ELSIF (state = "011") THEN
-				vendingEnable <= '1';
+				vendingEnable <= '1';		-- in vending state, enable vending
+				
+				change0 <= changeOut0;		-- connect all outputs to vending unit
+				change1 <= changeOut1;
+				change2 <= changeOut2;
+				runTotal0 <= runTotalOut0;
+				runTotal1 <= runTotalOut1;
+				runTotal2 <= runTotalOut2;
+				total0 <= displayOut0;
+				total1 <= displayOut1;
+				total2 <= displayOut2;
 			ELSIF (state = "100") THEN
-				-- do free stuff
+				freeEnable <= '1';			-- in free state, enable free vending
+				
+				change0 <= "ZZZZ";			-- connect all outputs to free unit
+				change1 <= "ZZZZ";			-- (the 'Z's output are likely not safe in the BCD, so they're set manually here)
+				change2 <= "ZZZZ";
+				runTotal0 <= "ZZZZ";
+				runTotal1 <= "ZZZZ";
+				runTotal2 <= "ZZZZ";
+				total0 <= "ZZZZ";
+				total1 <= "ZZZZ";
+				total2 <= "ZZZZ";
 			END IF;
 		END IF;
 	END PROCESS;
 	
+	-- set the allDone signal based on whether the unit running is done or not
 	allDone <= '1' when (programmingDone = '1' AND state = "001") OR (vendingDone = '1' AND state = "010") OR (freeDone = '1' AND state = "100") else
 				  '0';
-				  
-	currentPrice <= unsigned(currentPriceSTD);	-- convert current price from STD_LOGIC_VECTOR to UNSIGNED
 	
+	currentPrice <= unsigned(currentPriceSTD);	-- convert currentPrice from STD_LOGIC_VECTOR to UNSIGNED
+	
+	currentPriceReal <= "101"*(currentPrice);
+	changeOutReal <= "101"*(changeOut);
+	totalInsertedOutReal <= "101"*(totalInsertedOut);
+	
+	-- SRAM unit
 	SRAM_0 : SRAM PORT MAP(clock => clock, address => prod, wren => writeEnableOut,
 								  data => std_logic_vector(dataOut), q => currentPriceSTD);
 	
+	-- BCD unit for display
+	toBCD_0 : toBCD PORT MAP(binaryIn => currentPriceReal(7 DOWNTO 0),
+									 decimal0 => displayOut0, decimal1 => displayOut1, decimal2 => displayOut2);
+	-- BCD units for vending
+	toBCD_1 : toBCD PORT MAP(binaryIn => changeOutReal(7 DOWNTO 0),
+									 decimal0 => changeOut0, decimal1 => changeOut1, decimal2 => changeOut2);
+									 
+	toBCD_2 : toBCD PORT MAP(binaryIn => totalInsertedOutReal(7 DOWNTO 0),
+									 decimal0 => runTotalOut0, decimal1 => runTotalOut1, decimal2 => runTotalOut2);
+	
+	-- Programming unit
 	programmingUnit_0 : programmingUnit PORT MAP(clock => clock, reset => reset, set => set, enable => programmingEnable,
 																product => prod, QDN => std_logic_vector'(Q & D & N), writeEnable => writeEnableOut,
 																address => addressOut, data => dataOut, done => programmingDone);
-	
+	-- Vending unit
 	vendingUnit_0 : vendingUnit PORT MAP(clock => clock, reset => reset, enable => vendingEnable,
 													 price => currentPrice, QDN => std_logic_vector'(Q & D & N),
 													 totalInserted => totalInsertedOut, change => changeOut, done => vendingDone);
-	
+	-- Free unit
 	freeUnit_0 : freeUnit PORT MAP(clock => clock, enable => freeEnable,
 											 price => currentPrice, QDN => std_logic_vector'(Q & D & N),
 											 totalInserted => totalInsertedFree, change => changeFree, done => freeDone);
